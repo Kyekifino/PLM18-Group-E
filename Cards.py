578b2bd6e35eae2b150283385a27a86a49bf7460
Cassandra Keesee <curlyq43@gmail.com>
2018-02-20 12:35:14 -0500

    Add files via upload
    Card, Deck, and Card Stack classes implemented. Deal method is tentative- Might need to be changed depending on Player class.

---
+++ Cards.py
@@ -0,0 +1,53 @@
+# For use in setting up various card games.
+from random import shuffle
+
+class Card(object):
+    """Represents a card object."""
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+class cardStack(object):
+    """Represents any such stack of cards."""
+
+    def __init__(self):
+        self.stack = []
+        self.isVisible = true
+
+class Deck(cardStack):
+    """Represents a deck of cards."""
+
+    def __init__(self):
+        self.cards = []
+        self.isVisible = true
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def changeVisibility(self):
+        if self.isVisible:
+            self.isVisible = false
+        else :
+            self.isVisible = true
+
+    def deal(self, size, Player):
+        for num in Range(0,size):
+            Player.hand[i] = self.cards.pop()
+
+    def draw(self):
+        return self.cards.pop()
+
+    def addToDeck(self):
+        self.cards.append(card)
+
+    def fillDeck(self):
+        if not cards:
+            for suit in range(4):
+                for rank in range(1, 14):
+                    card = Card(suit, rank)
+                    self.cards.append(card)
+
+    def resetDeck(self):
+        self.cards = []
+
